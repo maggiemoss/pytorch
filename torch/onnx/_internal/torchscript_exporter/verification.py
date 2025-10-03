448e7280fbb325b1f925b086886639847105d090
Maggie Moss <maggiebmoss@gmail.com>
2025-10-03 16:05:29 -0700

    Add suppressions, fix lint. Add suppressions

--- torch/onnx/_internal/torchscript_exporter/verification.py
+++ torch/onnx/_internal/torchscript_exporter/verification.py
@@ -239,6 +239,7 @@ def _compare_onnx_pytorch_outputs_in_np(
             if acceptable_error_percentage:
                 error_percentage = 1 - np.sum(
                     np.isclose(ort_out, pt_out, rtol=options.rtol, atol=options.atol)
+                    # pyrefly: ignore  # missing-attribute
                 ) / np.prod(ort_out.shape)
                 if error_percentage <= acceptable_error_percentage:
                     warnings.warn(
@@ -247,8 +248,10 @@ def _compare_onnx_pytorch_outputs_in_np(
                         f"within acceptable range {acceptable_error_percentage}."
                     )
                     continue
+            # pyrefly: ignore  # missing-attribute
             if ort_out.dtype == np.uint8 or ort_out.dtype == np.int8:
                 warnings.warn("ONNX output is quantized")
+            # pyrefly: ignore  # missing-attribute
             if pt_out.dtype == np.uint8 or pt_out.dtype == np.int8:
                 warnings.warn("PyTorch output is quantized")
             raise
